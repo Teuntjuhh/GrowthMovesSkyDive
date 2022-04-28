2e4331609130abed6f6b8e65bc0ec59c7ef69a23
L.L <galleonidas@gmail.com>
2022-04-28 10:57:58 +0200

    Added Equation and TextMeshPro

---
+++ SkyDive/Assets/Scripts/Equation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equation : MonoBehaviour
+{
+    public enum Operator { Add, Subtract, Multiply, Divide}
+
+    int maxNumber = 10;
+
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int givenAnswer = 0;
+
+
+
+
+}
